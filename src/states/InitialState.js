d5d9c3596daa60b488483b733dce31748d0b6376
passoul <pascal.soulier@gmail.com>
2021-06-16 16:10:09 +0200

    refactor :  create a folder and file for app state

---
+++ src/states/InitialState.js
@@ -0,0 +1,27 @@
+  // define initial state of user
+  export const InitialState = {
+    loading: false,
+    movies: [
+      {
+        title: 'The Avengers',
+        image: 'http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg',
+        comment: 'New York blows up in this!'
+      },
+      {
+        title: 'Dark City',
+        image: 'https://i.chzbgr.com/full/5569379584/hA96709E0/',
+        comment: 'This looks mysterious. Cool!'
+      },
+      {
+        title: 'Hot Tub Time Machine',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg',
+        comment: 'Someone said this was fun. Maybe!'
+      },
+  ],
+  watchlist: localStorage.getItem("watchlist")
+    ? JSON.parse(localStorage.getItem("watchlist"))
+    : [],
+  watched: localStorage.getItem("watched")
+    ? JSON.parse(localStorage.getItem("watched"))
+    : [],
+}
